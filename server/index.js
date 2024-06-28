9810872033350f879a6a8643102bd247b773d007
Eduardo Henrique <eduardo.hbds@gmail.com>
2024-06-28 20:11:15 -0300

    Added the cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-
+// pra que serve o cors?
 const app = express();
+
 app.use(cors());
+app.use(express.json());
 
 app.listen(1300, ()=> {
     console.log("Server is running in port 1300");
