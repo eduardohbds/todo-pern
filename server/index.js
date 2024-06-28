2d68e5aca3d83c28c2acf98e7197848bf2863127
Eduardo Henrique <eduardo.hbds@gmail.com>
2024-06-28 16:11:21 -0300

    delete nodemon

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
-import express from "express";
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 app.listen(1300, ()=> {
     console.log("Server is running in port 1300");
