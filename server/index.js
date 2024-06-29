5f113eae7bac799a514224f45e884b3d5c4a923e
Eduardo Henrique <eduardo.hbds@gmail.com>
2024-06-28 22:36:34 -0300

    feat: index modified

--- server/index.js
+++ server/index.js
@@ -46,6 +46,31 @@ app.get("/todos/:id", async (req,res) =>
         }
 });
 
+app.put("/todos/:id",async (req,res) => {
+    try {
+        const {id} = req.params;
+        const {description} = req.body;
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2",
+            [description,id]
+        );
+        res.json("Todo was updated")
+    } catch (error) {
+        console.error(err.message);
+    }
+});
+
+app.delete("/todos/:id",async (req,res) => {
+    try {
+        const {id} = req.params;
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1",
+            [id]
+        );
+        res.json("Todo was deleted");
+    } catch (error) {
+        console.error(err.message);
+    }
+});
+
 app.listen(1300, ()=> {
     console.log("Server is running in port 1300");
 });
