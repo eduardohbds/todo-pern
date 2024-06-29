f20f4124ade38f13743bece77048498afc5d962c
Eduardo Henrique <eduardo.hbds@gmail.com>
2024-06-29 01:04:42 -0300

    Changes  in the components

--- client/src/component/EditTodo.jsx
+++ client/src/component/EditTodo.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
+import axios from "axios";
 
-const EditTodo = () => {
+const EditTodo = ({todo}) => {
+  const [description, setDescription] = useState(todo.description);
+  const updateDescription = async () => { 
+    try {
+      const body = {description};
+      const response = await axios.put("http://localhost:1300/todos/${id}",{ description: body })
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
   return (
-    <div>EditTodo</div>
+    <div>
+
+    </div>
   )
 }
 
--- client/src/component/ListTodo.jsx
+++ client/src/component/ListTodo.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from "axios"
 const ListTodo = () => {
-  return (
-    <div>ListTodo</div>
-  )
+    const [todos, setTodos] = useState([]);
+
+    const deleteTodo = async () => { 
+        try {
+            const response = await axios.delete("http://localhost:1300/todos/${id}")
+            setTodos(todos.filter((todo) => { todo.todo_id !== id }))
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+    const getTodo = async () => { 
+        try {
+            const response = await axios.get("http://localhost:1300/todos/")
+            const jsonData = await response.data()
+            setTodos(jsonData)
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+    useEffect(() => {
+    getTodo();
+    }, [])
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default ListTodo
